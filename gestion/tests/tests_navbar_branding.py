effb6402b679d5da3ac23ff69dca5dd8682a9c01
CM Soluciones y herramientas Contables <ernestomp505@gmail.com>
2026-08-09 23:05:08 -0500

    test: agregar cobertura de logo personalizado en el navbar
    tests_navbar_branding.py solo cubria el caso del icono por defecto.
    Se agrega el caso espejo con logo subido, siguiendo el mismo patron
    que LoginBrandingConLogoPersonalizadoTest.

--- gestion/tests/tests_navbar_branding.py
+++ gestion/tests/tests_navbar_branding.py
@@ -1,6 +1,19 @@
+import base64
+import shutil
+import tempfile
+
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase, override_settings
 from django.urls import reverse
+
+from gestion.models import ConfiguracionEmpresa
+
+PNG_1PX = base64.b64decode(
+    b'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII='
+)
+
+TEMP_MEDIA_ROOT = tempfile.mkdtemp()
 
 
 class NavbarBrandingTest(TestCase):
@@ -12,3 +25,27 @@ class NavbarBrandingTest(TestCase):
         response = self.client.get(reverse('gestion:dashboard'))
         self.assertContains(response, 'navbar-logo-chip')
         self.assertContains(response, 'avenida-chile-icono.svg')
+
+
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
+class NavbarBrandingConLogoPersonalizadoTest(TestCase):
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='navuser2', password='testpass123')
+        self.client.force_login(self.user)
+
+    def test_navbar_usa_logo_subido_cuando_existe(self):
+        logo_file = SimpleUploadedFile('logo.png', PNG_1PX, content_type='image/png')
+        ConfiguracionEmpresa.objects.create(
+            nombre_empresa='Empresa Con Logo',
+            nit_empresa='900123456-3',
+            representante_legal='Juana Ejemplo',
+            activo=True,
+            logo=logo_file,
+        )
+        response = self.client.get(reverse('gestion:dashboard'))
+        self.assertContains(response, 'empresa/logos/logo')
